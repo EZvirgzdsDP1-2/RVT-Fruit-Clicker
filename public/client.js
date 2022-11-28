84aff6633f8c407707263225b69f3eeeff85d0d9
EZvirgzdsDP1-2 <ed.zvir04@inbox.lv>
2022-11-28 07:57:50 +0200

    Added tree which now can be clicked

--- public/client.js
+++ public/client.js
@@ -4,7 +4,7 @@ import { GUI } from './buildDatGui/dat.gui.module.js'; //Dat.gui library
 import { GLTFLoader }  from './jsmThree/loaders/GLTFLoader.js'; //GLTF loader
 import { FBXLoader }  from './jsmThree/loaders/FBXLoader.js'; //FBX loader
 import { InteractionManager } from './threeInteractive/three.interactive.js';//three.interactive
-import * as TWEEN from './TweenJsDist/tween.esm.js'; //Tween.js  ,"tween": "./TweenJsDist/tween.esm.js" { Tween }
+import * as TWEEN from './TweenJsDist/tween.esm.js'; //Tween.js
 
 //GUI change text to different
 GUI.TEXT_OPEN = "Open fruit upgrade";
@@ -20,20 +20,63 @@ const gui = new GUI( {  resizable : false,
 
 getDatGuiContainer.appendChild(gui.domElement);
 
+//Geometry and mesh for cube to be clicked on
+const cubeGeometry = new THREE.BoxGeometry();
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    opacity: 0,
+    transparent: true,
+});
+
+const cubeTree = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cubeTree.scale.x = 2;
+cubeTree.scale.y = 1.7;
+cubeTree.scale.z = 2;
+
+//Load manager
+const manager = new THREE.LoadingManager();
+
+//Texutre loader
+const textureLoader = new THREE.TextureLoader();
 
 //Initialize object loader
-const fbxLoader = new FBXLoader();
-const gltfLoader = new GLTFLoader();
+const fbxLoader = new FBXLoader(manager);
+const gltfLoader = new GLTFLoader(manager);
 
 //Init THREE JS scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 
-//Experimental, delete later when not needed
+//Count variables. Important for the gameplay.
 let numberCount = 0;
 
-//Object variables which will be used when playing the game
-let appleTree; //= new THREE.Object3D()
+//Texture loader initializations
+let appleTreeTextures = await textureLoader.loadAsync('./resourcesObjects/appleTree/textures/gltf_embedded_0.png'); 
+appleTreeTextures.flipY = false;
+
+//Object variables which will be used when playing the game. This part of the code could be optimized more and use a smarter method of assigning.
+let appleTreeModel = await gltfLoader.loadAsync('./resourcesObjects/appleTree/source/appletree.glb', function(gltf) {
+
+  gltf.name = 'appleTree';
+
+  /*gltf.scene.traverse( (o) => {
+    if(o.isMesh)
+    {
+      o.material.map = appleTreeTextures;
+    }
+  } );
+  */
+  return gltf;
+
+}, undefined, function (error)
+{
+  console.error(error);
+} ); 
+
+let applesModels = [];
+
+//Tween Js functionality
+appleTreeModel.userData.isTweening = false; //Related to the animation the tree will do. Is true when animation is in progress and false when not
 
 //Add funtion to GUI interface
 const addFuntionToButton = {
@@ -45,22 +88,11 @@ const addFuntionToButton = {
 };
 
 //Set camera position
-camera.position.z = 3;
+camera.position.z = 5;//3
 camera.position.y = 1;
 camera.rotation.x = -0.2;
 
-//Test geometry and mesh for cube (To be replaced by game objects)
-const cubeGeometry = new THREE.BoxGeometry();
-const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    opacity: 0.1,
-    transparent: true,
-});
 
-const cubeTree = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-cubeTree.userData.isTweening = false; //Related to the animation the tree will do.
-//Is true when animation is in progress and false when not
 
 //Create renderer to render objects in the scene
 const renderer = new THREE.WebGLRenderer();
@@ -81,15 +113,30 @@ window.addEventListener('resize',
     false
 )
 
+//Loading manager functions
+
+manager.onStart = function(url, itemsLoaded, itemsTotal)
+{
+  console.log('Started loading file: '+ url +'. \n Loaded '+ itemsLoaded +' of '+ itemsTotal +' files.');
+};
+
+manager.onLoad = function()
+{
+  console.log('Loading Complete');
+};
+
+manager.onError = function(url)
+{
+  console.log( 'There was an error loading ' + url );
+};
+
 //Function object loader
 
-function addFbxObject(path, name) //FBX file format
+function addFbxObject(path, nameObj) //FBX file format
 {
   fbxLoader.load(path, function(fbx) {
 
-    let objectToAdd = new THREE.Object3D();
-
-    objectToAdd = fbx;
+    fbx.name = nameObj;
 
     scene.add( fbx );
 
@@ -100,20 +147,18 @@ function addFbxObject(path, name) //FBX file format
 
 }
 
-function addGltfObject(path, name) //GLTF and GLB format files
+function addGltfObject(path, nameObj) //GLTF and GLB format files
 {
   gltfLoader.load(path, function(gltf) {
 
-    //let objectToAdd = new THREE.Object3D();
-
-    //objectToAdd = gltf;
+    gltf.name = nameObj;
 
     scene.add( gltf.scene );
 
   }, undefined, function (error)
   {
     console.error(error);
-  } );
+  } ); 
 }
 
 //Decrease and increase cube size (later tree size)
@@ -121,23 +166,23 @@ function clickAnimationTree()
 {
   console.log("Times clicked: " + ++numberCount);
 
-  if(cubeTree.userData.isTweening) return;
+  if(appleTreeModel.userData.isTweening) return;
 
-  let tweenDeflate = new TWEEN.Tween(cubeTree.scale).to({
-    x:0.8,
-    y:0.8,
-    z:0.8
+  let tweenDeflate = new TWEEN.Tween(appleTreeModel.scene.scale).to({
+    x:2.16,
+    y:2.16,
+    z:2.88
 
   }, 90).easing(TWEEN.Easing.Back.In).onStart( ()=>{
       cubeTree.userData.isTweening = true;
   });
  
-  let tweenInflate = new TWEEN.Tween(cubeTree.scale).to({
-    x:1,
-    y:1,
-    z:1
+  let tweenInflate = new TWEEN.Tween(appleTreeModel.scene.scale).to({
+    x:2.7,
+    y:2.7,
+    z:3.6
   }, 50).onComplete( ()=>{
-    cubeTree.userData.isTweening = false;
+    appleTreeModel.userData.isTweening = false;
   });
   
   
@@ -149,8 +194,14 @@ function clickAnimationTree()
 //Dat.gui function
 function addGUI()
 {
-  gui.add(cubeTree.scale, 'x', 0, 3).name("Scale x axis"); //first experiment
+  //gui.add(cubeTree.scale, 'x', 0, 3).name("Scale x axis"); //first experiment
   gui.add(addFuntionToButton, 'addNumber');
+}
+
+//Get objects and then assign them to variables
+function assignVariablesObjects()
+{
+  
 }
 
 //Add test cube to scene, later to be replaced by in-game objects
@@ -160,23 +211,38 @@ function addTree()
 
   interactionThree.add(cubeTree);
 
-  appleTree; 
-  
-  addGltfObject('./resourcesObjects/appleTree/source/appletree.glb');
+  scene.add(appleTreeModel.scene);
 
-  //scene.add(appleTree.scene);
+  /*appleTreeModel.scene.traverse ( (o) => {
+    if(o.isMesh)
+    {
+      o.material.map = appleTreeTextures;
+    }
+  } );
+  */
+
+  
+  
+  cubeTree.add(appleTreeModel.scene);
   //'./resourcesObjects/apple/source/apple.fbx'
 
-  //appleTree.scale.x = 3;
-  //appleTree.scale.y = 3;
-  //appleTree.scale.z = 3;
+  //Adjusting location of apple tree
+  cubeTree.position.set(0,0.3,0);
+
+  appleTreeModel.scene.scale.x = 2.7;
+  appleTreeModel.scene.scale.y = 2.7;
+  appleTreeModel.scene.scale.z = 3.6;
+
+  appleTreeModel.scene.position.x = 0;
+  appleTreeModel.scene.position.y = 0;
+  appleTreeModel.scene.position.z = -1;
 }
 
 //Animate the scene
 function animate()
 {
     requestAnimationFrame(animate);
-    //cubeSpinSlow();
+    treeSpinSlow();
 
     TWEEN.update();
     interactionThree.update();
@@ -203,6 +269,7 @@ function init()
   
   addTree();
   addGUI();
+  assignVariablesObjects(); // Experimental
   animate();
 }
 
